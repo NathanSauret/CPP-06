2aa99c9c1c8ed36e8ba0a01b270d51115fa960ac
Nathan Sauret <nsauret@f2r6s15.clusters.42paris.fr>
2025-08-01 13:45:29 +0200

    modified ex02 functions identify

--- ex02/Src/functions.cpp
+++ ex02/Src/functions.cpp
@@ -25,101 +25,35 @@ static void identify(Base *Test)
 		std::cout << "\tbase is B" << std::endl;
 	else if (dynamic_cast<C *>(Test))
 		std::cout << "\tbase is C" << std::endl;
-}
+	else
+		std::cout << "\tbase is unknown" << std::endl;
 
-static int i = 0;
-static std::string classes[] = {"A", "B", "C"};
+}
 
 static void identify(Base &Test)
 {
-	while (i < 3)
+	try
 	{
-		try
-		{
-			if (i == 0)
-				Base &unused = dynamic_cast<A &>(Test);
-			else if (i == 1)
-				Base &unused = dynamic_cast<B &>(Test);
-			else if (i == 2)
-				Base &unused = dynamic_cast<C &>(Test);
-		}
-		catch (std::exception &e)
-		{
-			i++;
-			identify(Test);
-			return;
-		}
-		std::cout << "\tbase is ";
-		switch (i)
-		{
-			case (0):
-			{
-				std::cout << "A";
-				break;
-			}
-			case (1):
-			{
-				std::cout << "B";
-				break;
-			}
-			case (2):
-			{
-				std::cout << "C";
-				break;
-			}
-		}
-		std::cout << std::endl;
-		i = 0;
-		break;
+		Base &unused = dynamic_cast<A &>(Test);
+		std::cout << "\tbase is A" << std::endl;
+		return ;
 	}
+	catch (std::exception &e){}
+
+	try
+	{
+		Base &unused = dynamic_cast<B &>(Test);
+		std::cout << "\tbase is B" << std::endl;
+		return ;
+	}
+	catch (std::exception &e){}
+
+	try
+	{
+		Base &unused = dynamic_cast<C &>(Test);
+		std::cout << "\tbase is C" << std::endl;
+		return ;
+	}
+	catch (std::exception &e){}
+	std::cout << "\tbase is unknown" << std::endl;
 }
-
-// static void identify(Base &Test)
-// {
-// 	bool	found = 0;
-// 	int		i = 0;
-
-// 	while (i < 3)
-// 	{
-// 		void *foo = NULL; // only to initialize the unused var
-// 		Base &unused = (Base &)foo; // only to prevent the -Werror from triggering for unused value of the casts
-// 		try
-// 		{
-// 			if (i == 0)
-// 			{
-// 				unused = dynamic_cast<A &>(Test);
-// 				found = 1;
-// 			}
-// 			else if (i == 1)
-// 			{
-// 				unused = dynamic_cast<B &>(Test);
-// 				found = 1;
-// 			}
-// 			else if (i == 2)
-// 			{
-// 				unused = dynamic_cast<C &>(Test);
-// 				found = 1;
-// 			}
-// 			(void)unused;
-// 		}
-// 		catch (std::exception &e)
-// 		{
-// 			i++;
-// 			identify(Test);
-// 		}
-// 		if (found)
-// 		{
-// 			switch (i)
-// 			{
-// 				case (0):
-// 					std::cout << "\tA";
-// 				case (1):
-// 					std::cout << "\tB";
-// 				case (2):
-// 					std::cout << "\tC";
-// 			}
-// 			std::cout << " is the identified type" << std::endl;
-// 			break;
-// 		}
-// 	}
-// }
